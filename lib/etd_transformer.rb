368b011b51f55291d09da7ae19ad04a763840a4b
Bess Sadler <bess.sadler@princeton.edu>
2021-04-05 18:14:48 -0400

    Start building intended class structure

---
+++ lib/etd_transformer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'etd_transformer/vireo/export'
+
+##
+# Transform Princeton senior theses (a.k.a. ETDs -- electronic theses and dissertations).
+# Take the thesis as received from Vireo and augment it so that it can be ingested for
+# long term stewardship.
+module EtdTransformer
+end
