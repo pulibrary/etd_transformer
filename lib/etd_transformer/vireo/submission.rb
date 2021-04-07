ad328dc3f3f8a54ce7a7f5bb530adc60eae1d70d
Bess Sadler <bess.sadler@princeton.edu>
2021-04-07 13:42:29 -0400

    Find the original filename as listed in Excel

--- lib/etd_transformer/vireo/submission.rb
+++ lib/etd_transformer/vireo/submission.rb
@@ -6,10 +6,23 @@ module EtdTransformer
     # A single DSpace Archive item downloaded from Vireo. It represents a
     # single thesis.
     class Submission
-      attr_reader :row
+      attr_reader :row, :student_id, :student_name, :primary_document
 
       def initialize(row)
         @row = row
+        parse_row
+      end
+
+      ##
+      # Pull data out of the Excel row and assign it to instance variables for ease of access.
+      def parse_row
+        @student_id = @row['Student ID']
+        @student_name = @row['Student name']
+        @primary_document = @row['Primary document']
+      end
+
+      def original_pdf
+        File.basename(@primary_document)
       end
     end
   end
--- spec/etd_transformer/vireo/submission_spec.rb
+++ spec/etd_transformer/vireo/submission_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe EtdTransformer::Vireo::Submission do
   let(:ve) { EtdTransformer::Vireo::Export.new(ve_department_name) }
   let(:dataspace_import_directory) { "#{$fixture_path}/mock-exports" }
   let(:ve_asset_directory) { "#{vireo_export_directory}/#{ve_department_name}" }
+  let(:submission) do
+    submission = nil
+    ve.metadata.simple_rows.each_with_index do |row, index|
+      next if index.zero? # skip the header row
+      next unless row['Status'] == 'Approved'
+
+      submission = described_class.new(row)
+      break
+    end
+    submission
+  end
 
   around do |example|
     vireo_export_dir_pre_test = ENV['VIREO_EXPORT_DIRECTORY']
@@ -17,15 +28,14 @@ RSpec.describe EtdTransformer::Vireo::Submission do
     ENV['DATASPACE_IMPORT_DIRECTORY'] = dataspace_import_dir_pre_test
   end
 
-  it 'has a row from Excel' do
-    submission = nil
-    ve.metadata.simple_rows.each_with_index do |row, index|
-      next if index.zero? # skip the header row
-      next unless row['Status'] == 'Approved'
-
-      submission = EtdTransformer::Vireo::Submission.new(row)
-      break
-    end
+  it 'gets data from a row from Excel' do
     expect(submission.row).to be_instance_of(Hash)
+    expect(submission.student_id).to eq '961251996'
+    expect(submission.student_name).to eq 'Cheon, Janice'
+    expect(submission.primary_document).to eq 'http://thesis-central.princeton.edu//submit/review/1584978277IgrBDmmfiXo/8297/CHEON-JANICE-THESIS.pdf'
+  end
+
+  it 'finds the original pdf document' do
+    expect(submission.original_pdf).to eq 'CHEON-JANICE-THESIS.pdf'
   end
 end
