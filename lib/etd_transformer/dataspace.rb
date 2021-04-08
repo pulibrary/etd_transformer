25c33a3f8b4ca2501cc273535839fd8c3b758d7d
Bess Sadler <bess.sadler@princeton.edu>
2021-04-08 15:13:41 -0400

    Start creating Dataspace module

---
+++ lib/etd_transformer/dataspace.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'dataspace/import'
+
+##
+# A department worth of content to be imported into DataSpace
+module Dataspace
+end
---
+++ lib/etd_transformer/dataspace/import.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'byebug'
+require 'creek'
+require 'fileutils'
+
+module EtdTransformer
+  module Dataspace
+    # A department worth of theses ready for import to Dataspace.
+    # A Dataspace::Import has a department, a directory of Dataspace::Submission
+    # objects, and a metadata spreadsheet in Excel.
+    class Import
+    end
+  end
+end
