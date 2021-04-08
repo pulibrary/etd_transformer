a01c968d3821907983927173f6ca706748c0befe
Kevin McElwee <26414213+kmcelwee@users.noreply.github.com>
2021-04-08 13:03:44 -0400

    Find original PDF (#20)
    * Find the original pdf file
    * Create source_files_directory method
    * Checked the pdf path exists and unzip dspace archive
    * Add cover page to assets
    
    Co-authored-by: Bess Sadler <bess.sadler@princeton.edu>

--- lib/etd_transformer/vireo/submission.rb
+++ lib/etd_transformer/vireo/submission.rb
@@ -6,9 +6,14 @@ module EtdTransformer
     # A single DSpace Archive item downloaded from Vireo. It represents a
     # single thesis.
     class Submission
-      attr_reader :row, :student_id, :student_name, :primary_document
+      attr_reader :row,
+                  :student_id,
+                  :student_name,
+                  :primary_document,
+                  :id
 
-      def initialize(row)
+      def initialize(vireo_export:, row:)
+        @vireo_export = vireo_export
         @row = row
         parse_row
       end
@@ -19,10 +24,26 @@ module EtdTransformer
         @student_id = @row['Student ID']
         @student_name = @row['Student name']
         @primary_document = @row['Primary document']
+        @id = @row['ID']
+      end
+
+      ##
+      # Create the path to the source files
+      def source_files_directory
+        "#{vireo_export_directory}/#{@vireo_export.department_name}/DSpaceSimpleArchive/submission_#{@id}"
       end
 
       def original_pdf
         File.basename(@primary_document)
+      end
+
+      def original_pdf_exists?
+        full_path = File.join(source_files_directory, original_pdf)
+        File.exist?(full_path)
+      end
+
+      def vireo_export_directory
+        @vireo_export.vireo_export_directory
       end
     end
   end
