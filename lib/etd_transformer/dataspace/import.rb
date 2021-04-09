c981683424e743363b1a92220f9dd628dd2f4404
Bess Sadler <bess.sadler@princeton.edu>
2021-04-09 15:51:37 -0400

    Start fleshing out Dataspace module

--- lib/etd_transformer/dataspace/import.rb
+++ lib/etd_transformer/dataspace/import.rb
@@ -10,6 +10,13 @@ module EtdTransformer
     # A Dataspace::Import has a department, a directory of Dataspace::Submission
     # objects, and a metadata spreadsheet in Excel.
     class Import
+      attr_reader :department_name
+
+      ##
+      # @param [String] department_name The name of the department.
+      def initialize(department_name)
+        @department_name = department_name
+      end
     end
   end
 end
