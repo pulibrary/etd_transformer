cd3f9ce1407b0c358d18ca5930b891aab9ea35d7
Bess Sadler <bess.sadler@princeton.edu>
2021-04-29 16:59:21 -0400

    Add embargo spreadsheet to command line CLI

--- spec/etd_transformer/cli_spec.rb
+++ spec/etd_transformer/cli_spec.rb
@@ -3,8 +3,9 @@
 RSpec.describe EtdTransformer::Cli do
   let(:cli) { described_class.new }
   let(:input_dir) { "#{$fixture_path}/mock-downloads/German" }
-  let(:output_dir) { "#{$fixture_path}/export/German" }
-  let(:options) { { input: input_dir, output: output_dir } }
+  let(:output_dir) { "#{$fixture_path}/cli_invoked" }
+  let(:embargo_spreadsheet) { "#{$fixture_path}/mock-downloads/embargo_spreadsheet_with_netids.xlsx" }
+  let(:options) { { input: input_dir, output: output_dir, embargo_spreadsheet: embargo_spreadsheet } }
 
   context 'without arguments' do
     it 'prints a help message' do
@@ -13,12 +14,19 @@ RSpec.describe EtdTransformer::Cli do
   end
 
   context 'with required arguments' do
+    before do
+      allow(EtdTransformer::Transformer).to receive(:transform)
+    end
     it 'has an input directory' do
       expect { cli.invoke(:process, [], options) }.to output(/#{input_dir}/).to_stdout
     end
 
     it 'has an output directory' do
       expect { cli.invoke(:process, [], options) }.to output(/#{output_dir}/).to_stdout
+    end
+
+    it 'has an embargo spreadsheet' do
+      expect { cli.invoke(:process, [], options) }.to output(/#{embargo_spreadsheet}/).to_stdout
     end
   end
 end
