ab627e2aecc7fff8c3802dc642279da919cb7ddc
Bess Sadler <bess.sadler@princeton.edu>
2021-05-20 14:45:46 -0400

    Style fix

--- lib/etd_transformer/vireo/export.rb
+++ lib/etd_transformer/vireo/export.rb
@@ -70,6 +70,7 @@ module EtdTransformer
           next if index.zero? # skip the header row
           next unless row['Status'] == 'Approved'
           raise "This spreadsheet does not have the required ID column" if row['ID'].nil?
+
           @approved_submissions[row['ID']] = EtdTransformer::Vireo::Submission.new(asset_directory: @asset_directory, row: row)
         end
         @approved_submissions
