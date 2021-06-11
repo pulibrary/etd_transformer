611f8bcf5cc5059f78b29ae499f6d65830f5e374
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-06-11 11:20:54 -0400

    Add department name and xml file to class

--- lib/etd_transformer/proquest/collection_mapper.rb
+++ lib/etd_transformer/proquest/collection_mapper.rb
@@ -3,6 +3,16 @@
 module EtdTransformer
   module Proquest
     class CollectionMapper
+      attr_reader :department_name, :xml_file
+
+      def initialize(department_name)
+        @department_name = department_name
+        @xml_file_path = xml_file_path
+      end
+
+      def xml_file_path
+        File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'assets', 'collection_map.xml'))
+      end
     end
   end
 end
--- spec/etd_transformer/proquest/collection_mapper_spec.rb
+++ spec/etd_transformer/proquest/collection_mapper_spec.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe EtdTransformer::Proquest::CollectionMapper do
-  it 'initializes' do
-    collection_mapper = EtdTransformer::Proquest::CollectionMapper.new
+  let(:department_name) { 'Architecture' }
+
+  it 'has a department and points to an xml file' do
+    collection_mapper = EtdTransformer::Proquest::CollectionMapper.new(:department_name)
     expect(collection_mapper).to be_instance_of(EtdTransformer::Proquest::CollectionMapper)
+    expect(collection_mapper.department_name).to eq(:department_name)
+    expect(File.file? collection_mapper.xml_file_path).to be true
   end
 end
