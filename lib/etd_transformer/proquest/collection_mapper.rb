32092861abf0b9368f64f8cbcda18b6d819eea32
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-06-11 11:44:51 -0400

    Generate collection hash

--- lib/etd_transformer/proquest/collection_mapper.rb
+++ lib/etd_transformer/proquest/collection_mapper.rb
@@ -2,16 +2,34 @@
 
 module EtdTransformer
   module Proquest
+    ##
+    # Generate the proquest department -> dataspace handle mapper from XML
     class CollectionMapper
-      attr_reader :department_name, :xml_file
+      attr_reader :department_name, :xml_file, :mapper
 
+      # TODO: this generates the mapper for everyone. It shouldn't accept a department_name
+      # We care only about the mapper.
       def initialize(department_name)
         @department_name = department_name
         @xml_file_path = xml_file_path
+        @mapper = generate_collection_mapper
       end
 
       def xml_file_path
         File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'assets', 'collection_map.xml'))
+      end
+
+      def generate_collection_mapper
+        mapper = {}
+        doc = File.open(xml_file_path) { |f| Nokogiri::XML(f) }
+
+        doc.xpath('.//collection').each do |el|
+          department = el.xpath('name').text
+          handle = el.xpath('identifier').text
+          mapper[department] = handle
+        end
+
+        mapper
       end
     end
   end
--- spec/etd_transformer/proquest/collection_mapper_spec.rb
+++ spec/etd_transformer/proquest/collection_mapper_spec.rb
@@ -2,11 +2,19 @@
 
 RSpec.describe EtdTransformer::Proquest::CollectionMapper do
   let(:department_name) { 'Architecture' }
+  let(:collection_handle) { '88435/dsp01f4752g74r' }
 
   it 'has a department and points to an xml file' do
-    collection_mapper = EtdTransformer::Proquest::CollectionMapper.new(:department_name)
+    collection_mapper = EtdTransformer::Proquest::CollectionMapper.new(department_name)
     expect(collection_mapper).to be_instance_of(EtdTransformer::Proquest::CollectionMapper)
-    expect(collection_mapper.department_name).to eq(:department_name)
-    expect(File.file? collection_mapper.xml_file_path).to be true
+    expect(collection_mapper.department_name).to eq(department_name)
+    expect(File.file?(collection_mapper.xml_file_path)).to be true
+  end
+
+  it 'generates a hash from the xml file' do
+    collection_mapper = EtdTransformer::Proquest::CollectionMapper.new(department_name)
+    expect(collection_mapper.mapper).to be_instance_of(Hash)
+    mapper = collection_mapper.mapper
+    expect(mapper[department_name]).to eq(collection_handle)
   end
 end
