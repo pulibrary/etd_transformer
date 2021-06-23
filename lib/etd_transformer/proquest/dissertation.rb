bf12f585777a37d8be489375c4f4ec58138a739e
Bess Sadler <bess.sadler@princeton.edu>
2021-06-23 14:57:11 -0400

    Fleshing out more dublin core fields

--- lib/etd_transformer/proquest/dissertation.rb
+++ lib/etd_transformer/proquest/dissertation.rb
@@ -20,6 +20,38 @@ module EtdTransformer
             xml.dcvalue(element: 'contributor', qualifier: 'author') do
               xml.text author
             end
+            xml.dcvalue(element: 'title') do
+              xml.text title
+            end
+            xml.dcvalue(element: 'description', qualifier: 'abstract') do
+              xml.text abstract
+            end
+            xml.dcvalue(element: 'contributor', qualifier: 'advisor') do
+              xml.text advisor
+            end
+            xml.dcvalue(element: 'contributor', qualifier: 'other') do
+              xml.text "#{department} Department"
+            end
+            xml.dcvalue(element: 'date', qualifier: 'created') do
+              xml.text accept_date
+            end
+            xml.dcvalue(element: 'date', qualifier: 'issued') do
+              xml.text comp_date
+            end
+            xml.dcvalue(element: 'format', qualifier: 'mimetype') do
+              xml.text "application/pdf"
+            end
+            xml.dcvalue(element: 'language', qualifier: 'iso') do
+              xml.text iso_language
+            end
+            xml.dcvalue(element: 'type') do
+              xml.text "Academic dissertations (Ph.D.)"
+            end
+            keywords.each do |keyword|
+              xml.dcvalue(element: 'subject', qualifier: 'none') do
+                xml.text keyword
+              end
+            end
           end
         end
         builder
@@ -44,18 +76,60 @@ module EtdTransformer
       end
 
       ##
+      # Must be in "YYYY-MM-dd" format
+      def accept_date
+        proquest_date = metadata.xpath('//DISS_accept_date').text
+        Date.parse(proquest_date).strftime("%Y-%m-%d")
+      end
+
+      def comp_date
+        metadata.xpath('//DISS_comp_date').text
+      end
+
+      ##
       # Get the title from the XML
       def title
         metadata.xpath('/DISS_submission/DISS_description/DISS_title').text
       end
 
+      def abstract
+        metadata.xpath('//DISS_abstract').text.strip
+      end
+
+      def advisor
+        format_name(metadata.xpath('//DISS_advisor/DISS_name'))
+      end
+
       ##
       # Get the author from the XML
       def author
-        fname = metadata.xpath('/DISS_submission/DISS_authorship/DISS_author/DISS_name/DISS_fname').text
-        middle = metadata.xpath('/DISS_submission/DISS_authorship/DISS_author/DISS_name/DISS_middle').text
-        surname = metadata.xpath('/DISS_submission/DISS_authorship/DISS_author/DISS_name/DISS_surname').text
-        suffix = metadata.xpath('/DISS_submission/DISS_authorship/DISS_author/DISS_name/DISS_suffix').text
+        format_name(metadata.xpath('/DISS_submission/DISS_authorship/DISS_author/DISS_name'))
+      end
+
+      def iso_language
+        metadata.xpath('//DISS_language').text
+      end
+
+      ##
+      # Return an array of keywords
+      def keywords
+        words = []
+        keyword_elements = metadata.xpath('//DISS_keyword')
+        keyword_elements.each do |keyword_element|
+          keyword_element.text.split(",").each do |k|
+            words << k.strip
+          end
+        end
+        words
+      end
+
+      ##
+      # Given a DISS_name element, format it as a string
+      def format_name(diss_name_element)
+        fname = diss_name_element.xpath('DISS_fname').text
+        middle = diss_name_element.xpath('DISS_middle').text
+        surname = diss_name_element.xpath('DISS_surname').text
+        suffix = diss_name_element.xpath('DISS_suffix').text
         names = [fname, middle, surname, suffix]
         no_empty_names = names.reject(&:empty?)
         no_empty_names.join(' ').squeeze(" ").strip
